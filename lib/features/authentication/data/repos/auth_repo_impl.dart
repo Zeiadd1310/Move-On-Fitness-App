53b29c4324eb9668c7b3645c16083b04390f62f0
Zeiad Ramadan <zeiadramadan6@gmail.com>
2026-03-01 02:07:34 +0200

    auth repo implementation done

--- lib/features/authentication/data/repos/auth_repo_impl.dart
+++ lib/features/authentication/data/repos/auth_repo_impl.dart
@@ -1,5 +1,7 @@
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:move_on/core/errors/failures.dart';
+import 'package:move_on/core/utils/functions/api_service.dart';
 import 'package:move_on/features/authentication/data/models/forgot_password_model.dart';
 import 'package:move_on/features/authentication/data/models/logout_model.dart';
 import 'package:move_on/features/authentication/data/models/resend_otp_model.dart';
@@ -11,48 +13,24 @@ import 'package:move_on/features/authentication/data/models/verify_otp_model.dar
 import 'package:move_on/features/authentication/data/repos/auth_repo.dart';
 
 class AuthRepoImpl implements AuthRepo {
-  @override
-  Future<Either<Failure, ForgotPasswordModel>> forgotPassword({
-    required String email,
-  }) {
-    // TODO: implement forgotPassword
-    throw UnimplementedError();
-  }
+  final ApiService apiService;
 
-  @override
-  Future<Either<Failure, LogoutModel>> logout() {
-    // TODO: implement logout
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Either<Failure, ResendOtpModel>> resendOtp({required String email}) {
-    // TODO: implement resendOtp
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Either<Failure, ResetPasswordModel>> resetPassword({
-    required String newPassword,
-    required String confirmPassword,
-  }) {
-    // TODO: implement resetPassword
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Either<Failure, SendOtpModel>> sendOtp({required String email}) {
-    // TODO: implement sendOtp
-    throw UnimplementedError();
-  }
+  AuthRepoImpl(this.apiService);
 
   @override
   Future<Either<Failure, SigninModel>> signIn({
     required String email,
     required String password,
-  }) {
-    // TODO: implement signIn
-    throw UnimplementedError();
+  }) async {
+    try {
+      final data = await apiService.post(
+        endPoint: 'Account/login',
+        body: {'email': email, 'password': password},
+      );
+      return Right(SigninModel.fromJson(data));
+    } on DioException catch (e) {
+      return Left(ServerFailure.fromDioError(e));
+    }
   }
 
   @override
@@ -61,14 +39,112 @@ class AuthRepoImpl implements AuthRepo {
     required String email,
     required String password,
     required String confirmPassword,
-  }) {
-    // TODO: implement signUp
-    throw UnimplementedError();
+  }) async {
+    try {
+      final data = await apiService.post(
+        endPoint: 'Account/register',
+        body: {
+          'name': name,
+          'email': email,
+          'password': password,
+          'confirmPassword': confirmPassword,
+        },
+      );
+      return Right(SignupModel.fromJson(data));
+    } on DioException catch (e) {
+      return Left(ServerFailure.fromDioError(e));
+    }
   }
 
   @override
-  Future<Either<Failure, VerifyOtpModel>> verifyOtp({required String otp}) {
-    // TODO: implement verifyOtp
-    throw UnimplementedError();
+  Future<Either<Failure, ForgotPasswordModel>> forgotPassword({
+    required String email,
+  }) async {
+    try {
+      final data = await apiService.post(
+        endPoint: 'Account/forgot-password',
+        body: {'email': email},
+      );
+      return Right(ForgotPasswordModel.fromJson(data));
+    } on DioException catch (e) {
+      return Left(ServerFailure.fromDioError(e));
+    }
+  }
+
+  @override
+  Future<Either<Failure, SendOtpModel>> sendOtp({
+    required String email,
+  }) async {
+    try {
+      final data = await apiService.post(
+        endPoint: 'Account/send-otp',
+        body: {'email': email},
+      );
+      return Right(SendOtpModel.fromJson(data));
+    } on DioException catch (e) {
+      return Left(ServerFailure.fromDioError(e));
+    }
+  }
+
+  @override
+  Future<Either<Failure, ResendOtpModel>> resendOtp({
+    required String email,
+  }) async {
+    try {
+      final data = await apiService.post(
+        endPoint: 'Account/resend-otp',
+        body: {'email': email},
+      );
+      return Right(ResendOtpModel.fromJson(data));
+    } on DioException catch (e) {
+      return Left(ServerFailure.fromDioError(e));
+    }
+  }
+
+  @override
+  Future<Either<Failure, VerifyOtpModel>> verifyOtp({
+    required String otp,
+  }) async {
+    try {
+      final data = await apiService.post(
+        endPoint: 'Account/verify-otp',
+        body: {'otp': otp},
+      );
+      return Right(VerifyOtpModel.fromJson(data));
+    } on DioException catch (e) {
+      return Left(ServerFailure.fromDioError(e));
+    }
+  }
+
+  @override
+  Future<Either<Failure, ResetPasswordModel>> resetPassword({
+    required String newPassword,
+    required String confirmPassword,
+  }) async {
+    try {
+      final data = await apiService.post(
+        endPoint: 'Account/reset-password',
+        body: {
+          'newPassword': newPassword,
+          'confirmPassword': confirmPassword,
+        },
+      );
+      return Right(ResetPasswordModel.fromJson(data));
+    } on DioException catch (e) {
+      return Left(ServerFailure.fromDioError(e));
+    }
+  }
+
+  @override
+  Future<Either<Failure, LogoutModel>> logout() async {
+    try {
+      final data = await apiService.post(
+        endPoint: 'Account/logout',
+        body: {},
+      );
+      return Right(LogoutModel.fromJson(data));
+    } on DioException catch (e) {
+      return Left(ServerFailure.fromDioError(e));
+    }
   }
 }
